5b5f64d4e3a4556879aac93200c07ffb226998ab
AMullin11 <71042513+AMullin11@users.noreply.github.com>
2020-09-30 14:41:27 -0400

    Point and Click Controller
    Added control system where you move players by clicking on the map and cycle between them by pressing the space bar.

---
+++ Assets/Player_ClickMovement/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Player[] PlayerList;
+    int PlayerSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerList = GetComponentsInChildren<Player>(); //Makes a list of the children under this controller. Supports any number of players
+
+        PlayerSelected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Big thanks to the NavMesh lecture for this te c h   s k i l l
+        if(Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)){
+                    PlayerList[PlayerSelected].MoveToLocation(hit.point); //Tells the currently selected player to move to where you clicked
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            PlayerSelected = (PlayerSelected + 1) % PlayerList.Length; //cycles between players when you press space
+        }
+    }
+}
---
+++ Assets/Player_ClickMovement/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    //Navmesh stuff
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void MoveToLocation(Vector3 point){
+        agent.SetDestination(point);
+    }
+}
